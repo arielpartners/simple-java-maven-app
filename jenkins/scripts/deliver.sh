9abfa9bd7667c8849c1917e940d8d1a04393aafd
Ken Granderson <kgranderson@arielpartners.com>
2019-04-03 20:10:08 -0400

    bump

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-'/Program Files/Java/jdk-11.0.2/bin/java' -jar target/${NAME}-${VERSION}.jar
+${JAVA_HOME}/java -jar target/${NAME}-${VERSION}.jar
